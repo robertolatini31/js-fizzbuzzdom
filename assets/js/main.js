7977648e38d792d4d50a9c42b078edcf7c11b4ce
robertolatini31 <robertolatini31@gmail.com>
2022-03-28 13:34:55 +0200

    added rule to log fizz

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
 // Scrivi un programma che stampi in console i numeri da 1 a 100
 
 for (let i = 1 ; i <= 100 ; i++) {
-    console.log(i);
+
+    if ((i % 3) == 0) {
+        console.log('Fizz');
+    }
+    else {
+        console.log(i);
+    }
+    
 }
+
+// Per i multipli di 3 stampi “Fizz” al posto del numero
